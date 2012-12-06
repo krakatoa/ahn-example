99c894277963e3d31fbab3044ef0973bfe2f7b60
Fernando Alonso <krakatoa1987@gmail.com>
2012-12-06 11:44:24 -0300

    Updated manifests to install ruby 1.9.3-p194

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,25 +2,42 @@ $build_dir = "/vagrant/manifests"
 
 $rbenv_prefix = "/usr/local/"
 $ruby_version = "1.9.3-p194"
+$user         = "vagrant"
+$rbenv_group  = "rbenv"
+$profile_path_wide = "/etc/zsh/zshenv"
+$profile_path = "/home/vagrant/.zshrc"
 
 class ivr {
 
   include rbenv
   include ruby
 
-  rbenv::setup { "rbenv-setup":
+  rbenv::download { "rbenv-download": 
     prefix => $rbenv_prefix
   }
 
-  ruby::setup { "ruby-setup":
+  rbenv::setup { "rbenv-setup":
+    prefix => $rbenv_prefix,
+    profile_path => $profile_path,
+    user    => $user,
+    group  => $rbenv_group
+  }
   
+  # rbenv::setup { "rbenv-setup-wide":
+  #   prefix => $rbenv_prefix,
+  #   profile_path => $profile_path_wide
+  # }
+
+  ruby::setup { "ruby-setup":
   }
 
   rbenv::install { "rbenv-install":
-    ruby_version => $ruby_version
+    ruby_version => $ruby_version,
+    user          => $user,
+    group         => $rbenv_group
   }
 
-  Rbenv::Setup['rbenv-setup'] -> Ruby::Setup["ruby-setup"] -> Rbenv::Install['rbenv-install']
+  Rbenv::Download["rbenv-download"] -> Rbenv::Setup['rbenv-setup'] -> Ruby::Setup["ruby-setup"] -> Rbenv::Install['rbenv-install']
 }
 
 require 'ivr'
--- manifests/rbenv.pp
+++ manifests/rbenv.pp
@@ -1,35 +1,6 @@
-# == Class: rbenv
-#
-# Install rbenv.
-#
-# === Parameters
-#
-# $rbenv::params::repo_path
-#   The repository to clone from
-#   Defaults to 'git://github.com/sstephenson/rbenv.git'
-# $rbenv::params::repo_name
-#   The name of the new local directory to clone into
-#   Defaults to 'rbenv'
-# $rbenv::params::install_prefix
-#   The name of the existing parent directory to clone into
-#   Defaults to '/usr/local'
-# $rbenv::params::install_dir
-#   The full path to the newly created directory
-#   Defaults to $install_prefix/$repo_name
-#
-# === Variables
-#
-# === Examples
-#
-#  class { 'rbenv':  }
-#
 # === Authors
 #
-# Brendan O'Donnell <brendan.james.odonnell@gmail.com>
-#
-# === Copyright
-#
-# Copyright (C) 2012 Brendan O'Donnell
+# Fernando Alonso <fedario@gmail.com>, Brendan O'Donnell <brendan.james.odonnell@gmail.com>
 #
 class rbenv::params {
 
@@ -45,67 +16,109 @@ class rbenv::params {
 class rbenv {
 
   include rbenv::params
-  
-  define setup($prefix) {
+
+  define line($file, $line, $ensure = 'present') {
+    case $ensure {
+      default : { err ( "unknown ensure value ${ensure}" ) }
+      present: {
+        exec { "/bin/echo '${line}' >> '${file}'":
+          unless => "/bin/grep -qFx '${line}' '${file}'"
+        }
+      }
+      absent: {
+        exec { "/bin/grep -vFx '${line}' '${file}' | /usr/bin/tee '${file}' > /dev/null 2>&1":
+          onlyif => "/bin/grep -qFx '${line}' '${file}'"
+        }
+
+        # Use this resource instead if your platform's grep doesn't support -vFx;
+        # note that this command has been known to have problems with lines containing quotes.
+        # exec { "/usr/bin/perl -ni -e 'print unless /^\\Q${line}\\E\$/' '${file}'":
+        #     onlyif => "/bin/grep -qFx '${line}' '${file}'"
+        # }
+      }
+    }
+  }
+
+  define download($prefix) {
   
     exec { "git clone ${rbenv::params::repo_name}":
       command   => "git clone \
                     ${rbenv::params::repo_path} \
                     ${rbenv::params::install_dir}",
       path      => ["/usr/bin", $prefix],
-      creates   => $rbenv::params::install_dir,
-      notify => File['/etc/profile.d/rbenv.sh'],
-      before => File['/etc/profile.d/rbenv.sh']
-    }
-  
-    file { [
-      $rbenv::params::install_dir,
-      "${rbenv::params::install_dir}/plugins",
-      "${rbenv::params::install_dir}/shims",
-      "${rbenv::params::install_dir}/versions"
-    ]:
-      ensure    => directory,
-      owner     => 'root',
-      group     => 'root',
-      mode      => '0775'
-    }
-  
-    file { '/etc/profile.d/rbenv.sh':
-      ensure    => file,
-      content   => template("${build_dir}/rbenv.sh.erb"),
-      mode      => '0775',
-      require   => Exec["git clone ${rbenv::params::repo_name}"],
-      notify    => Exec["source rbenv.sh"],
-      subscribe => Exec["git clone ${rbenv::params::repo_name}"]
-    }
-  
-    exec { "source rbenv.sh":
-      command => "bash -c 'source /etc/profile.d/rbenv.sh'",
-      path => ["/bin", "/usr/bin", "/usr/local/bin"],
-      #user => $user,
-      #group => $group,
-      subscribe => File["/etc/profile.d/rbenv.sh"],
-      #notify => Exec["rbenv::compile ${user} ${ruby}"],
-      refreshonly => true
+      creates   => $rbenv::params::install_dir
     }
     
+  }
   
-    #Exec["git clone ${rbenv::params::repo_name}"] -> File['/usr/local/rbenv/bin/rbenv']
+  define setup($prefix, $profile_path, $user, $group) {
+    
+    file { "${rbenv::params::install_dir}":
+      group => $group,
+      mode => 774,
+      recurse => true
+    }
   
+    #file { [
+    #  $rbenv::params::install_dir,
+    #  "${rbenv::params::install_dir}/plugins",
+    #  "${rbenv::params::install_dir}/shims",
+    #  "${rbenv::params::install_dir}/versions"
+    #]:
+    #  ensure    => directory,
+    #  owner     => 'root',
+    #  group     => 'root',
+    #  mode      => '0775'
+    #}
+
+    group { "rbenv":
+      ensure => "present"
+    }
+
+    user { "set user rbenv group ${group}":
+      name => $user,
+      groups => $group
+    }
+  
+    # file { '/etc/profile.d/rbenv.sh':
+    #   ensure    => file,
+    #   content   => template("${build_dir}/rbenv.sh.erb"),
+    #   mode      => '0775',
+    #   #require   => Exec["git clone ${rbenv::params::repo_name}"],
+    #   #notify    => Line["source rbenv.sh"],
+    #   subscribe => Exec["git clone ${rbenv::params::repo_name}"]
+    # }
+
+    line { "source lines ${profile_path}":
+      file => $profile_path,
+      line => template("${build_dir}/rbenv.sh.erb")
+      #line => "source /etc/profile.d/rbenv.sh"
+    }
+  
+    exec { "source ${profile_path}":
+      command => "zsh -c 'source ${profile_path}'",
+      path => ["/bin", "/usr/bin", "/usr/local/bin"] #,
+      #subscribe => File["/etc/profile.d/rbenv.sh"],
+      #refreshonly => true
+    }
+
+    Line["source lines ${profile_path}"] -> Exec["source ${profile_path}"]
+    
   }
 
-  define install($ruby_version) {
+  define install($ruby_version, $user, $group) {
 
     exec { "rbenv::compile ${user} ${ruby_version}":
       command     => "rbenv install ${ruby_version}", # && touch ${root_path}/.rehash",
-      # timeout     => 0,
-      #user        => $user,
-      #group       => $group,
-      # environment => [ "HOME=${home_path}" ],
+      timeout     => 0,
+      user        => "root",
+      group       => $group,
+      #environment => [ "HOME=/home/vagrant" ],
+      environment => [ "HOME=/usr/local/rbenv"],
       #creates     => "${versions}/${ruby}",
-      path        => ["/usr/local/rbenv/bin"],
-      subscribe      => Exec["source rbenv.sh"],
-      refreshonly => true
+      path        => ["/bin", "/usr/bin", "/usr/local/bin", "/usr/local/rbenv/bin"]
+      #subscribe      => Exec["source rbenv.sh"],
+      #refreshonly => true
     }
 
     #exec { "rbenv::rehash ${user} ${ruby}":
